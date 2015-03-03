6f4a70ee44484def29b1067ce99e51fd8982f1ac
Dylan Staley <staley.dylan@gmail.com>
2015-03-02 18:16:08 -0600

    Permission statement check before checkout

--- app/controllers/kit.js
+++ app/controllers/kit.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    checkPermission: function() {
+      var controller = this;
+      var model = this.get('content');
+      if (model.kit.needs_permission) {
+        swal({
+          title: "Look out!",
+          text: model.kit.permission_statement,
+          type: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Continue'
+        }, function() {
+          controller.transitionToRoute('add-checkout', {queryParams: {kit: model.kit.id}});
+        });
+      }
+    }
+  }
 });
